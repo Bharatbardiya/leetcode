5d8dc18a1ef028f889b3d6eea5a863824720df7a
Bharat Bardiya <bharat.bardiya@e2enetworks.com>
2024-07-09 14:32:16 +0530

    daily challenge 9 july 2024 completed

---
+++ 1701.average-waiting-time.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1701 lang=cpp
+ *
+ * [1701] Average Waiting Time
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double ans = 0, temp = customers[0][0];
+        int n = customers.size();
+        for(auto customer : customers){
+            ans += customer[1];
+            if(temp > customer[0]){
+                ans += temp-customer[0];
+                temp += customer[1];
+            }
+            else {
+                temp = customer[0]+customer[1];
+            }
+        }
+        return ans/n;
+    }
+};
+// @lc code=end
+
